49e9041be2069817235d8f8af29ca2a20eed1266
Africk-Hunter <hunterafrick@gmail.com>
2024-04-02 16:15:28 -0700

    Main components structure + styling

--- src/Timer.js
+++ src/Timer.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './Timer.css';
 
+let timerStarted = false;
+let onSecondsRemaining = 1200;
+let offSecondsRemaining = 20;
+
 function Timer() {
   return ( 
 
@@ -28,17 +32,16 @@ function InnerTimer(){
 function TimeAsset(props){
 
     function getLabelType(){
-        if(props.label == "ON"){
+        if(props.label === "ON"){
             return "timeContainer on";
         } else {
             return "timeContainer off";
         }
     }
-
     return (
         <div class= { getLabelType() }>
-            <div class="time">{ props.time }</div>
-            <div class="label"> { props.label }</div>
+            <div>{ props.time }</div>
+            <div> { props.label }</div>
         </div>
         
     );
@@ -46,6 +49,43 @@ function TimeAsset(props){
 
 function ButtonContainer(){
 
+    if(timerStarted === false){
+        return(
+            <div class="buttonHolder">
+                <TimerButton label="START"/>
+            </div>
+        );
+    } else{
+        return(
+            <div class="buttonHolder">
+                <TimerButton label="PAUSE"/>
+                <TimerButton label="RESET"/>
+            </div>
+        );
+    }
+}
+
+function TimerButton(props){
+    function timerStart(){
+        timerStarted = !timerStarted;
+        console.log(timerStarted);
+    }
+
+    if(props.label === "START"){
+        return(
+            <div class="timerBtn start" onClick={timerStart}>{props.label}</div>
+        );
+    }
+    else if(props.label === "PAUSE"){
+        return(
+            <div class="timerBtn pause">{props.label}</div>
+        );
+    }
+    else if(props.label === "RESET"){
+        return(
+            <div class="timerBtn reset">{props.label}</div>
+        );
+    }   
 }
 
 export default Timer;
